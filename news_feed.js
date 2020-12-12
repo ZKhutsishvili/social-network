73d6468ffaecf6544c5b0cb92f0cdbde314fd840
zkhut16 <zkhut16@freeuni.edu.ge>
2020-12-13 00:25:18 +0400

    added pictures to post and made posts little prettier

--- news_feed.js
+++ news_feed.js
@@ -15,15 +15,27 @@ async function getUsers(){
 
 }
 
+async function getPictures(){
+    let response = await fetch("https://jsonplaceholder.typicode.com/photos");
+	if(response.ok){
+		let json = await response.json();
+		return json;
+	}
+
+}
+
 async function addPosts(posts, users){
-	users = await getUsers();
-	posts = await getPosts();
+	var users = await getUsers();
+	var posts = await getPosts();
+	var pics = await getPictures();
 	var feed = document.getElementsByClassName("middle");
 	var res = "";
 	for (var i = posts.length - 1; i >= 0; i--) {
 		res += "<div class='post'>"
-		res += "<div class='post-user'>" +users[i%10].name+ "</div>";
+		res += "<div class='post-user'> <img src='https://picsum.photos/300/200?random=" + i+"'alt='profile picture' class='prof-pic'>";
+		res += "<div class='fullname'>"+users[i%10].name+ "</div></div>";
 		res += "<div class='post-title'>" + posts[i].title + "</div>";
+		res += "<img src='https://picsum.photos/400/200?random=" + i + "' class='post-pic' alt='" + pics[i].title + "'>";
 		res += "</div>";
 	}
 	feed[0].innerHTML = res;
